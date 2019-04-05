4e08dc1608f128f47578b28058fd227895f5563a
David <562237291@qq.com>
2019-04-05 23:28:02 +0800

    Inheritance

--- TiCpp/Useful.h
+++ TiCpp/Useful.h
@@ -7,5 +7,5 @@ public:
 	X() { i = 0; }
 	void set(int ii) { i = ii; }
 	int read() const { return i; }
-	int permute() { return i = i * 47; }
+	int permute() { return  i = i * 47; }
 };// USEFUL_H ///:~
