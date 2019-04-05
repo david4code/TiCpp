13ae9adcefc4907c9d1e9816e6eeba9dbc676e8e
David <562237291@qq.com>
2019-04-05 23:09:30 +0800

    Composition

---
+++ TiCpp/Useful.h
@@ -0,0 +1,11 @@
+#pragma once
+//: C14:Useful.h
+// A class to reuse
+class X {
+	int i;
+public:
+	X() { i = 0; }
+	void set(int ii) { i = ii; }
+	int read() const { return i; }
+	int permute() { return i = i * 47; }
+};// USEFUL_H ///:~
