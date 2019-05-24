ba7740ecbb5ad22d5e9489a9d152380f756b5436
David <562237291@qq.com>
2019-05-24 10:05:34 +0800

    Chapter 16

--- TiCpp/Instrument4.cpp
+++ TiCpp/Instrument4.cpp
@@ -1,42 +1,29 @@
-//: C15:ObjectSlicing.cpp
-#include <iostream>
-#include <string>
+#include"require.h"
+#include<iostream>
 using namespace std;
-class Pet {
-	string pname;
+
+template<class T>
+class Array {
+	enum{size = 100};
+	T A[size];
 public:
-	Pet(const string& name) : pname(name) {}
-	Pet(const Pet& p) :pname(p.pname) {}
-	virtual string name() const { return pname; }
-	virtual string description() const {
-		return "This is " + pname;
-	}
-	~Pet() {
-		cout << "Destruct Pet" << endl;
+	T& operator[](int index) {
+		require(index >= 0 && index < size,
+			"Index out of range");
+		return A[index];
 	}
 };
-class Dog : public Pet {
-	string favoriteActivity;
-public:
-	Dog(const string& name, const string& activity)
-		: Pet(name), favoriteActivity(activity) {}
-	Dog(const Dog& d) :favoriteActivity(d.favoriteActivity), Pet(d) {
-	}
-	string description() const {
-		return Pet::name() + " likes to " +
-			favoriteActivity;
-	}
-	~Dog() {
-		cout << "Destruct Dog" << endl;
-	}
-};
-void describe(Pet p) { // Slices the object
-	cout << p.description() << endl;
-}
+
 int main() {
-	Pet p("Alfred");
-	Dog d("Fluffy", "sleep");
-	Dog d1(d);
-	describe(p);
-	describe(d);
-} ///:~
+	Array<int> ia;
+	Array<float> fa;
+	Array<float> *p = &fa;
+	for (int i = 0; i < 20; i++) {
+		ia[i] = i * i;
+		fa[i] = float(i) * 1.414f;
+	}
+	for (int j = 0; j < 20; j++)
+		cout << j << ": " << ia[j]
+		<< ", " << fa[j] << endl;
+	return 0;
+}
